8cba0ca58e339a656e2c1e9fee92353d0149d30a
qyh2023 <qyh2023@sjtu.edu.cn>
2025-10-02 15:33:55 +0800

    feat(IWDG): watch_dog

--- qyh1001/Core/Src/main.c
+++ qyh1001/Core/Src/main.c
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "iwdg.h"
 #include "tim.h"
 #include "gpio.h"
 
@@ -90,6 +91,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_TIM1_Init();
+  MX_IWDG_Init();
   /* USER CODE BEGIN 2 */
   HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
   //HAL_TIM_Base_Start(&htim1);
@@ -111,9 +113,9 @@ int main(void)
     uint32_t brightness = arr_value * sinf(4 * HAL_GetTick() / 1000.f) - 1;
     __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, brightness);
 
-		// if (HAL_GPIO_ReadPin(KEY_GPIO_Port, KEY_Pin)== GPIO_PIN_SET) {
-		// 		HAL_IWDG_Refresh(&hiwdg);
-		// }
+		if (HAL_GPIO_ReadPin(KEY_GPIO_Port, KEY_Pin)== GPIO_PIN_SET) {
+				HAL_IWDG_Refresh(&hiwdg);
+		}
 
     /* USER CODE END WHILE */
 
@@ -139,8 +141,9 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSI|RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLM = 6;
